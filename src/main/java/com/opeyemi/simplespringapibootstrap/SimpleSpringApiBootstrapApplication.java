79d7dde7073a2427b9d4c29f4e1f09df5cb9ce78
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2021-01-11 09:24:32 +0100

    Add pom.xml dependencies

---
+++ src/main/java/com/opeyemi/simplespringapibootstrap/SimpleSpringApiBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.opeyemi.simplespringapibootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringApiBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringApiBootstrapApplication.class, args);
+	}
+
+}
