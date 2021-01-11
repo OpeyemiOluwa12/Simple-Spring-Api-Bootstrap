b0053f598d3562e01677a16a66c2e802a146cecf
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2021-01-11 09:24:32 +0100

    Add pom.xml dependencies

---
+++ src/test/java/com/opeyemi/simplespringapibootstrap/SimpleSpringApiBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opeyemi.simplespringapibootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringApiBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
