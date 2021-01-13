65e64b89200e81afad78dc882612f193f2061163
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2021-01-13 22:31:33 +0100

    Add graphql functionality

--- src/main/java/com/opeyemi/simplespringapibootstrap/service/DogNotFoundException.java
+++ src/main/java/com/opeyemi/simplespringapibootstrap/service/DogNotFoundException.java
@@ -1,15 +1,40 @@
 package com.opeyemi.simplespringapibootstrap.service;
 
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Dog not found")
-public class DogNotFoundException extends RuntimeException {
+public class DogNotFoundException extends RuntimeException implements GraphQLError {
+
+    private Map<String, Object> extensions = new HashMap<>();
 
     public DogNotFoundException() {
+
+    }
+    public DogNotFoundException(String message, Long invalidDogId) {
+        super(message);
+        extensions.put("invalidDogId", invalidDogId);
     }
 
-    public DogNotFoundException(String message) {
-        super(message);
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getExtensions() {
+        return extensions;
+    }
+
+    @Override
+    public ErrorType getErrorType() {
+        return ErrorType.DataFetchingException;
     }
 }
