d4745bef3aa5c8ef25feacd5b7a2b1ae5f5eaeca
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2021-01-16 09:09:18 +0100

    Add Eureka server

--- src/main/java/com/opeyemi/simplespringapibootstrap/SimpleSpringApiBootstrapApplication.java
+++ src/main/java/com/opeyemi/simplespringapibootstrap/SimpleSpringApiBootstrapApplication.java
@@ -2,8 +2,10 @@ package com.opeyemi.simplespringapibootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SimpleSpringApiBootstrapApplication {
 
 	public static void main(String[] args) {
