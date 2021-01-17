7b05b6c4a33d38aea75d3bed4225c863707fa23b
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2021-01-17 21:03:46 +0100

    Add Basic authentication to api

--- src/main/java/com/opeyemi/simplespringapibootstrap/SimpleSpringApiBootstrapApplication.java
+++ src/main/java/com/opeyemi/simplespringapibootstrap/SimpleSpringApiBootstrapApplication.java
@@ -2,14 +2,13 @@ package com.opeyemi.simplespringapibootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+//@EnableEurekaServer
 public class SimpleSpringApiBootstrapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleSpringApiBootstrapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringApiBootstrapApplication.class, args);
+    }
 
 }
